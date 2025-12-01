e18351bbcabedbdba070a2d076d6ab3ab91571e8
Jörn Wagner <joern.wagner@explicatis.com>
2025-12-01 18:25:33 +0100

    Add bundle file

---
+++ src/DynamicsApiBundle.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Explicatis\DynamicsApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class DynamicsApiBundle extends AbstractBundle
+{
+}
